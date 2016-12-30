84a05af1fb7faaa205966e8d6392144be10271de
kolibrity <holubnycha@gmail.com>
2016-12-30 16:02:54 +0300

    Add files via upload

--- mishka/js/modernizr-custom.js
+++ mishka/js/modernizr-custom.js
@@ -12,3 +12,28 @@ navToggle.addEventListener('click', function() {
 		navMain.classList.remove('main-nav--opened');
 	}
 });
+
+
+/*Добавление и стилизация карты*/
+var map;
+var marker;
+var image;
+
+function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 59.9387942, lng: 30.3225036},
+		zoom: 13
+	});
+
+// Создаем маркер на карте
+	marker = new google.maps.Marker({
+// Определяем позицию маркера
+		position: {lat: 59.9387942, lng: 30.3225036},
+// Указываем на какой карте он должен появится. (На странице ведь может быть больше одной карты)
+		map: map,
+// Пишем название маркера - появится если навести на него курсор и немного подождать
+		title: 'HTML Academy',
+// Укажем свою иконку для маркера
+    icon: "../img/map-marker.png"
+	});
+}
